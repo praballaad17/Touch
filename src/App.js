9fcdd870dd728cf4d013afb835a91e48f2802777
praballaad17 <54369093+praballaad17@users.noreply.github.com>
2021-11-10 21:17:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { lazy, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ReactLoader from './components/loader';
 import * as ROUTES from './constants/routes';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
 
 import ProtectedRoute from './helpers/protected-route';
+import MainLoader from './loader/mainLoader';
 
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <UserContext.Provider value={{ user }}>
       <Router>
-        <Suspense fallback={<ReactLoader />}>
+        <Suspense fallback={<MainLoader />}>
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
