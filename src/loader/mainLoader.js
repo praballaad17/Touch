a13b1ac86914d5ca3cf0bb0cdf81118d83322263
praballaad17 <54369093+praballaad17@users.noreply.github.com>
2021-11-10 21:16:43 +0530

    Add files via upload

---
+++ src/loader/mainLoader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainLoader() {
+    return (
+        <div className="loader">
+            <div className="loader--heading">Touch</div>
+        </div>
+    )
+}
